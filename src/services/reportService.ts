f26c83ebbf85b783848242da4b7a99e49e7bd513
Ivo Depari <ivodepari1302@hotmail.com>
2026-01-19 15:47:23 -0300

    agrego funcion de eliminar reporte y descargar reporte

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -5,7 +5,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getAllReports = async () => {
   try {
     const response = await axios.get(`${API_URL}/report`);
-    console.log("Datos recibidos del GET:", response.data);
     return response.data;
   } catch (error) {
     console.error("Error al obtener reportes:", error);
@@ -13,13 +12,38 @@ export const getAllReports = async () => {
   }
 };
 
-export const createReportApi = async (reportData) => {
+export const createReportApi = async (reportData: any, type: string) => {
   try {
-    const response = await axios.post(`${API_URL}/report`, reportData);
-    console.log("Respuesta del servidor al CREAR:", response.data);
+    const response = await axios.post(
+      `${API_URL}/report`,
+      { ...reportData, type },
+      { responseType: "blob" }
+    );
     return response.data;
   } catch (error) {
     console.error("Error al crear reporte:", error);
+    return null;
+  }
+};
+
+export const downloadReportByIdApi = async (id: number, type: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/report/${id}/download`, {
+      responseType: "blob",
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al descargar historial:", error);
+    return null;
+  }
+};
+
+export const deleteReportApi = async (id: number) => {
+  try {
+    const response = await axios.delete(`${API_URL}/report/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar reporte:", error);
     return null;
   }
 };
