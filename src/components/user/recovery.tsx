e5df3386d1cd62985e1a976f9dce68a7ec95b5f9
FrancoDeavux <166407581+FrancoDevaux@users.noreply.github.com>
2026-01-22 19:09:49 -0300

    conexion front/back USER

--- src/components/user/recovery.tsx
+++ src/components/user/recovery.tsx
@@ -1,25 +1,31 @@
-import userMock from "../../mock/userMock.json";
 import { useState } from "react";
-import { TbMailForward, TbMailX } from "react-icons/tb";
+import { recoveryPassword } from "../../services/userService";
+import toast from "react-hot-toast";
 import "../../styles/user/recovery.css";
 export default function Recovery() {
   const [email, setemail] = useState("");
-  const handlesubmit = (event) => {
-    const user = userMock.find((u) => u.email === email);
-    if (user) {
-      //HACER LO DE LA NOTIFICACION ACA!!!!!
-      alert(
-        <>
-          <TbMailForward /> Se envio el correo
-        </>
+  const [loading, setLoading] = useState(false);
+  const handlesubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const response = await recoveryPassword(email);
+      console.log(response);
+      toast.success(
+        "Correo de recuperación enviado (Revisa tu bandeja de entrada)",
+        {
+          duration: 4000,
+        },
       );
-    } else {
-      event.preventDefault();
-      alert(
-        <>
-          <TbMailX /> Email no encontrado
-        </>
-      );
+    } catch (error: any) {
+      console.error(error);
+      if (error.response && error.response.status === 404) {
+        toast.error("El correo electrónico no está registrado");
+      } else {
+        toast.error("Error al enviar la solicitud. Intenta más tarde.");
+      }
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -33,8 +39,8 @@ export default function Recovery() {
           value={email}
           onChange={(e) => setemail(e.target.value)}
         />
-        <button type="submit" className="button-recovery">
-          Enviar Mail
+        <button type="submit" className="button-recovery" disabled={loading}>
+          {loading ? "Enviando..." : "Enviar Mail"}
         </button>
       </form>
     </div>
